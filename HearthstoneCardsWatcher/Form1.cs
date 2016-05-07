f5cc5bea9d792e6a08d30c116e044ab380b56314
Roman <gremkil@gmail.com>
2016-05-08 01:38:24 +0300

    *косяки

--- HearthstoneCardsWatcher/Form1.cs
+++ HearthstoneCardsWatcher/Form1.cs
@@ -1,35 +1,18 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json;
+using CoreLib;
 
 namespace HearthstoneCardsWatcher
 {
 	public partial class Form1 : Form
 	{
-		private ConcurrentBag<CardInfo> mInfos = new ConcurrentBag<CardInfo>();
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
-		{
-		}
-
-		void mWather_Exception(Exception e)
-		{
-			MessageBox.Show(e.Message);
-		}
-
-		async void mWather_FileChanged(string text)
 		{
 		}
 	}
